0c49f3b277deb5dc96504c273d01736c1e6219d1
Spyke Lionel <59184392+spykelionel@users.noreply.github.com>
2025-10-06 14:21:49 +0100

    Update verify.js

--- backend/auth/verify.js
+++ backend/auth/verify.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
     next();
   } catch (error) {
       return res.status(401).json({
-          message: "Auth failed"
+          message: "Authentication failed"
       })
   }
 };
